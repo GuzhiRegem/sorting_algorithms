2905f6c99de34680b1d9a1a191d2bf683190c861
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-10-25 16:07:00 -0700

    commit

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,6 +9,11 @@
  */
 void swap(int *array, size_t idx1, size_t idx2)
 {
+	int tmp;
+
+	tmp = array[idx1];
+	array[idx1] = array[idx2];
+	array[idx2] = tmp;
 }
 /**
  * shell_sort - compares two nodes
@@ -18,7 +23,7 @@ void swap(int *array, size_t idx1, size_t idx2)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t sep = 1, i;
+	size_t sep = 1, i, tmp;
 
 	if (!array)
 		return;
@@ -29,10 +34,18 @@ void shell_sort(int *array, size_t size)
 	while (sep > 1)
 	{
 		sep = sep / 3;
-		printf("separation: %ld\n", sep);
 		for(i = 0; i < (size - sep); i++)
 		{
-			printf("%d and %d\n", array[i], array[i + sep]);
+			tmp = i;
+			while (array[tmp] > array[tmp + sep])
+			{
+				swap(array, tmp, tmp + sep);
+				if (tmp >= sep)
+					tmp -= sep;
+				else
+					break;
+			}
 		}
+		print_array(array, size);
 	}
 }
